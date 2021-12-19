900eea3ed727c64554cade5535d2531b7834951f
Veljko <vd0911@student.uni-lj.si>
2021-12-19 19:44:39 +0100

    SearchList nije završen

--- app/src/main/java/com/example/salaris/UserWorkhoursActivity.java
+++ app/src/main/java/com/example/salaris/UserWorkhoursActivity.java
@@ -1,8 +1,11 @@
 package com.example.salaris;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 public class UserWorkhoursActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class UserWorkhoursActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_workhours);
+
+        initSearchWidgets();
+    }
+
+    private void initSearchWidgets(){
+        SearchView searchView = (SearchView) findViewById(R.id.SearchList);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
     }
 }
